4c3a1535ce2fe3c86aefe218ddc55d52818c3c69
sumitk-hub1999 <skatyal1999@gmail.com>
2022-12-24 15:04:50 +0530

    single alarm functionality

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const selection = document.querySelectorAll("select");
 const currenttime = document.querySelector("h1");
 const AlarmBtn = document.querySelector("button");
+const ClearBtn = document.getElementById("cl-b");
 const content = document.querySelector(".content");
 var gif = document.getElementById("clock");
 let alarmTime;
 let ringtone = new Audio("./files/Goofy cartoon sounds.mp3");
-
+let isAlarmSet = false;
 //creating selections to select time
 for (let i = 12; i > 0; i--) {
   i = i < 10 ? "0" + i : i;
@@ -70,7 +71,20 @@ function setAlarm() {
   ) {
     return alert("enter valid time");
   }
+  isAlarmSet = true;
   alarmTime = time;
   // content.classList.add("disable");
 }
+
 AlarmBtn.addEventListener("click", setAlarm);
+
+function clearAlarm() {
+  if (isAlarmSet) {
+    alarmTime = "";
+    gif.src = "./files/alarm-clock-svgrepo-com.svg";
+    ringtone.pause();
+
+    return (isAlarmSet = false);
+  }
+}
+ClearBtn.addEventListener("click", clearAlarm);
